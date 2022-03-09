f4ebe83c2f7569fdee0f05460c308cf349bed302
Mark Howe <mvahowe@gmail.com>
2022-03-09 11:11:23 +0100

    Try non-binary zip

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const freeze = async pk => {
     const zip = new JSZip();
     for (const docSet of docSets) {
         const stringified = JSON.stringify(pk.serializeSuccinct(docSet));
-        zip.file(docSet, stringified, {binary: true});
+        zip.file(docSet, stringified, {binary: false});
     }
     return zip.generateAsync({type: "base64"});
 }
@@ -17,7 +17,7 @@ const thaw = async (pk, frozen, selectorFunc, idFunc) => {
     await zip.loadAsync(frozen, {base64: true});
     const contentPromises = [];
     zip.forEach((path, zipEntry) => {
-        const content = zipEntry.async('binarystring');
+        const content = zipEntry.async('text');
         contentPromises.push(content);
     });
     await Promise.all(contentPromises)
